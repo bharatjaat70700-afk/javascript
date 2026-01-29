0a57b98f853d035ed6dc4bd24d8321a6a347271c
unknown <bharatjaat70700@gmail.com>
2026-01-29 11:07:43 +0530

    java

---
+++ script.js
@@ -0,0 +1,37 @@
+// let user={
+//     fullname:"bharat bainiwal",
+//     address:{
+//         city:"gurugram",
+//         state:"haryana"
+//     },
+//     mobileno:1234567890,
+//     favcolor:["black","white","blue"],
+//     demo:function(){
+//         return "demo function";
+//     }
+
+// }
+// console.log(user.fullname,user.mobile)
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
